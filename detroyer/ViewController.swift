d8574d218d121c5569779ab730e2b6071d0e1fef
Daniel Trask <d.trask@lecreuset.co.jp>
2017-03-29 09:45:35 +0900

    add var

--- detroyer/ViewController.swift
+++ detroyer/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +24,4 @@ class ViewController: UIViewController {
 
 
 }
-
+    var = 0
